cef282647d4c44a068a55964adbf7dda9a31292f
James McDaniel <icathaid@gmail.com>
2018-10-10 23:20:00 -0700

    pushing for submit

--- edit-file.js
+++ edit-file.js
@@ -2,17 +2,18 @@
 
 const fs = require('fs');
 
+//  i put my callback and file path out here to keep things straight in my head and reuse them later
 const bill = './files/test.txt';
-//  i put my callback out here to keep things straight in my head
 const ted = (err, data) => {
   if (err) { console.log('oh no an error in ted', err); }
   else {
-    console.log(data);
+    console.log('data read from file     ', data);
+    // i can log out the data asynchronously, but i can't figure out how to return it so i can save it to a variable
     // return data;
   }
 };
 
-let rando = Math.floor(Math.random() * 100);
+const rando = Math.floor(Math.random() * 100);
 
 const read = (file, callback) => {
   fs.readFile(file, 'utf8', (err, data) => {
@@ -23,19 +24,25 @@ const read = (file, callback) => {
   });
 };
 
-read(bill, ted);
-
-const addRandomNumber = (file, callback) => {
+const saveFile = (file, callback) => {
   read(file, (err, data) => {
     if (err) {
       console.log('oh no an error in addRandomNumber', err);
     }
     else {
+      console.log('data read from file     ', data);
       let addRando = data + '   ' + rando;
       callback(undefined, addRando);
     }
   });
-
 };
 
-addRandomNumber(bill, ted);
+
+let rufus = (err, data) => {
+  fs.writeFile(bill, data, (err) => {
+    if(err){ console.log('oh no there was an error inside rufus', err);}
+    else { console.log('data written to file     ', data); }
+  });
+};
+
+saveFile(bill, rufus);
